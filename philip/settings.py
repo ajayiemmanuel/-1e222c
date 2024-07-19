9d8e1099f4ae77c90362d0f1021fc97e1daac138
ajayiemmanuel <86613616+ajayiemmanuel@users.noreply.github.com>
2024-07-19 20:55:38 +0100

    settings.py

--- philip/settings.py
+++ philip/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'philip.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'djrRBGdRrvtcFzsmyFcnuNSUUGiQjYro',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '43042',
     }
 }
 
